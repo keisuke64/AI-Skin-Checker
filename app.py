56fb4a7306e020270c92d0c887efaeb3333e07ea
keisuke64 <166226112+keisuke64@users.noreply.github.com>
2026-07-29 14:26:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ app = Flask(__name__)
 gemini_client = genai.Client()
 
 # --- DB初期化をグローバルスコープ（または app.py の読み込み時）で実行 ---
-with app.app_context():
-    init_db()
+
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
@@ -80,6 +79,7 @@ def init_db():
         );
         ''')
         db.commit()
+init_db()
 
 def generate_skincare_advice(acne_count, skincare, sleep, diet):
     """
